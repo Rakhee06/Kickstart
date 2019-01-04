15d279305d4d73b445e291722ea4def4dc65354f
Rakhee Singh <rakhee@Rakhees-MacBook-Pro.local>
2019-01-03 19:09:28 -0500

    Modidied the listening port.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { createServer } = require('http');
 const next = require('next');
 const routes = require('./routes');
+const port = process.env.PORT || 3000;
 
 const app = next({
     dev: process.env.NODE_ENV !== 'production'
@@ -9,9 +10,9 @@ const app = next({
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    createServer(handler).listen(process.env.PORT || 3000, (error) => {
+    createServer(handler).listen(port, (error) => {
        if(error) throw error;
        // console.log('Ready on localhost:3000');
-        console.log("App running on Port " + process.env.PORT + ' or ' + " 3000 ");
+        console.log("App running on Port " + process.env.PORT );
     });
 });
